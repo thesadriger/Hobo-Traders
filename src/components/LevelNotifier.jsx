fed8daadb0b50a038755a5d8b750e7ae9115c56a
thesadriger <64961875+thesadriger@users.noreply.github.com>
2024-10-12 11:22:42 +0300

    Update LevelNotifier.jsx
    Переместил уведомление о новых уровнях в нижнюю часть

--- src/components/LevelNotifier.jsx
+++ src/components/LevelNotifier.jsx
@@ -1,8 +1,9 @@
 // src/components/LevelNotifier.jsx
 import React, { useEffect, useRef } from 'react';
-import { message } from 'antd';
+import { notification } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { resetIndicators } from '@/store/slices/indicatorsSlice';
+import './LevelNotifier.css'; // Импортируем файл стилей
 
 const LevelNotifier = () => {
   const dispatch = useDispatch();
@@ -16,14 +17,17 @@ const LevelNotifier = () => {
 
     if (currentFloor > previousFloor) {
       dispatch(resetIndicators());
-      message.info(`Поздравляем! Вы достигли нового уровня: ${currentFloor}`);
+      notification.open({
+        message: `Поздравляем! Вы достигли нового уровня: ${currentFloor}`,
+        placement: 'bottom',
+        className: 'custom-notification', // Добавляем кастомный класс
+      });
     }
 
-    // Обновляем предыдущий уровень
     prevLevelRef.current = level;
   }, [level, dispatch]);
 
-  return null; // Этот компонент не рендерит ничего
+  return null;
 };
 
 export default LevelNotifier;
